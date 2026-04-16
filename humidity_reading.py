89ff021cd6cc75d188a5ba34f7f419318552aae3
nisharanipraveen <nisharanid717@gmail.com>
2026-04-16 14:45:51 -0700

    fixed some errors

--- humidity_reading.py
+++ humidity_reading.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from .Humidity import Humidity
 
 
-humidity = Humidity()
+humidity = Humidity(0, datetime.now())
 def read_humidity():
     # Add code to read humidity from the sensor and return a Humidity object
     return humidity
